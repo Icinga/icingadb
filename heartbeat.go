6dceb4dab3d613ce8618a6c6e9ecde42b72868f6
Jean Flach <jean.flach@icinga.com>
2019-02-22 15:21:11 +0100

    Add heartbeat broker

---
+++ heartbeat.go
@@ -0,0 +1,48 @@
+package icingadb_ha_lib
+
+import (
+	"crypto/sha1"
+	"encoding/json"
+	"git.icinga.com/icingadb-connection"
+	log "github.com/sirupsen/logrus"
+	)
+
+// Compute SHA1
+func Sha1bytes(bytes []byte) []byte {
+	hash := sha1.New()
+	hash.Write(bytes)
+	return hash.Sum(nil)
+}
+
+
+func IcingaEventsBroker(rdb *icingadb_connection.RDBWrapper, chEnv chan *icingadb_connection.Environment, chErr chan error) {
+	log.Info("Starting Events broker")
+
+	subscription := rdb.Rdb.Subscribe()
+	defer subscription.Close();
+
+
+	if err := subscription.Subscribe(
+		"icinga:config:dump", "icinga:config:delete", "icinga:config:update", "icinga:stats"); err != nil {
+		chErr <- err
+	}
+
+	for {
+		msg, err := subscription.ReceiveMessage()
+		if err != nil {
+			chErr <- err
+		}
+
+		switch msg.Channel {
+		case "icinga:stats":
+			var unJson interface{} = nil
+			if err = json.Unmarshal([]byte(msg.Payload), &unJson); err != nil {
+				chErr <- err
+			}
+
+			environment := unJson.(map[string]interface{})["IcingaApplication"].(map[string]interface{})["status"].(map[string]interface{})["icingaapplication"].(map[string]interface{})["app"].(map[string]interface{})["environment"].(string)
+			env := &icingadb_connection.Environment{Name: environment, ID: Sha1bytes([]byte(environment))}
+			chEnv <- env
+		}
+	}
+}
