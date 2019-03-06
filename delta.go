ac51720636f0cbb7b7d6e9766771d345a705778d
Noah Hilverling <noah.hilverling@icinga.com>
2019-03-06 16:28:44 +0100

    Add delta.go

---
+++ delta.go
@@ -0,0 +1,30 @@
+package icingadb_utils
+
+func Delta(a []string, b []string) ([]string, []string, []string) {
+	maintained := make([]string, 0)
+	dismissed := make([]string, 0)
+	hash := make(map[string]bool)
+
+	for _, item := range a {
+		hash[item] = true
+	}
+
+	for _, item := range b {
+		if hash[item] {
+			maintained = append(maintained, item)
+			delete(hash, item)
+		} else {
+			dismissed = append(dismissed, item)
+		}
+	}
+
+	introduced := make([]string, len(hash))
+
+	i := 0
+	for k := range hash {
+		introduced[i] = k
+		i++
+	}
+
+	return introduced, maintained, dismissed
+}
