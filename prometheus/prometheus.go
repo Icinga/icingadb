caa8017870d62a7104dfdf4024124aa1f52e903c
Noah Hilverling <noah.hilverling@icinga.com>
2019-03-21 10:09:42 +0100

    Add Prometheus

---
+++ prometheus/prometheus.go
@@ -0,0 +1,13 @@
+package prometheus
+
+import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func HandleHttp(addr string, chErr chan error) {
+	http.Handle("/metrics", promhttp.Handler())
+	log.Infof("Serving metrics at http://%s/metrics", addr)
+	chErr <- http.ListenAndServe(addr, nil)
+}
