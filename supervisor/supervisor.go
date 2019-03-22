cc797f64ee77a25b0195a9bd26b3d02ae7f98db8
Noah Hilverling <noah.hilverling@icinga.com>
2019-03-22 11:23:31 +0100

    Use env in config sync

--- supervisor/supervisor.go
+++ supervisor/supervisor.go
@@ -3,13 +3,14 @@ package supervisor
 import (
 	"git.icinga.com/icingadb/icingadb-connection"
 	"git.icinga.com/icingadb/icingadb-json-decoder"
-	"git.icinga.com/icingadb/icingadb-ha"
+	"sync"
 )
 
 type Supervisor struct {
-	ChErr chan error
-	ChEnv chan *icingadb_ha.Environment
-	ChDecode chan *icingadb_json_decoder.JsonDecodePackages
-	Rdbw *icingadb_connection.RDBWrapper
-	Dbw  *icingadb_connection.DBWrapper
+	ChErr 		chan error
+	ChDecode 	chan *icingadb_json_decoder.JsonDecodePackages
+	Rdbw		*icingadb_connection.RDBWrapper
+	Dbw  		*icingadb_connection.DBWrapper
+	EnvId 		[]byte
+	EnvLock		*sync.Mutex
 }
