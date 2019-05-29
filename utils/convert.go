f31a68d0b425a7eb65dd5e779264396dd8be5b58
Noah Hilverling <noah.hilverling@icinga.com>
2019-05-29 13:59:51 +0200

    Remove unused/duplicate functions

--- utils/convert.go
+++ utils/convert.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"crypto/sha1"
 	"encoding/hex"
 )
 
@@ -12,7 +11,7 @@ var (
 	}
 )
 
-//Checksum converts a hex string to a byte array
+// Checksum converts a hex string to a byte array
 func Checksum(s string) []byte {
 	c, err := hex.DecodeString(s)
 	if err != nil {
@@ -22,40 +21,7 @@ func Checksum(s string) []byte {
 	return c
 }
 
-func Sha1(s string) []byte {
-	h := sha1.New()
-	h.Write([]byte(s))
-	bs := h.Sum(nil)
-
-	return bs
-}
-
-/**
- * Convert the given []byte to a [20]byte
- *
- * @param  in  []byte  Input
- *
- * @returns  out  [20]byte  Output
- */
-func Bytes2checksum(in []byte) (out [20]byte) {
-	copy(out[:], in)
-	return
-}
-
-/**
- * Convert the given [20]byte to a []byte
- *
- * @param  in  [20]byte  Input
- *
- * @returns  out  []byte  Output
- */
-func Checksum2bytes(in [20]byte) (out []byte) {
-	out = make([]byte, 20)
-	copy(out, in[:])
-	return
-}
-
-
+// DecodeChecksum coverts a byte array into a hex string
 func DecodeChecksum(c []byte) string {
 	return hex.EncodeToString(c)
 }
