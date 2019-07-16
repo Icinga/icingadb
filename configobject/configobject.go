702fdfe93beff46a0c44e1fbd09b7d9b0af1f5f4
Noah Hilverling <noah.hilverling@icinga.com>
2019-07-16 15:08:59 +0200

    Allow objects without checksum

--- configobject/configobject.go
+++ configobject/configobject.go
@@ -7,6 +7,7 @@ import (
 type ObjectInformation struct {
 	ObjectType 		string
 	RedisKey		string
+	HasChecksum		bool
 	Factory    		connection.RowFactory
 	BulkInsertStmt 	*connection.BulkInsertStmt
 	BulkDeleteStmt 	*connection.BulkDeleteStmt
