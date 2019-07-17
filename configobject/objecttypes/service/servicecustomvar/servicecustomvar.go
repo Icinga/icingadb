1dcf6db42c1021c21622d77605a7439710f1344f
Noah Hilverling <noah.hilverling@icinga.com>
2019-07-17 15:45:53 +0200

    Add various customvars

---
+++ configobject/objecttypes/service/servicecustomvar/servicecustomvar.go
@@ -0,0 +1,69 @@
+package servicecustomvar
+
+import (
+	"git.icinga.com/icingadb/icingadb-main/configobject"
+	"git.icinga.com/icingadb/icingadb-main/connection"
+	"git.icinga.com/icingadb/icingadb-main/utils"
+)
+
+var (
+	ObjectInformation configobject.ObjectInformation
+	Fields         = []string{
+		"id",
+		"service_id",
+		"customvar_id",
+		"env_id",
+	}
+)
+
+type ServiceCustomvar struct {
+	Id						string 		`json:"id"`
+	ServiceId				string		`json:"object_id"`
+	CustomvarId 			string 		`json:"customvar_id"`
+	EnvId           		string		`json:"env_id"`
+}
+
+func NewServiceCustomvar() connection.Row {
+	c := ServiceCustomvar{}
+	return &c
+}
+
+func (c *ServiceCustomvar) InsertValues() []interface{} {
+	v := c.UpdateValues()
+
+	return append([]interface{}{utils.Checksum(c.Id)}, v...)
+}
+
+func (c *ServiceCustomvar) UpdateValues() []interface{} {
+	v := make([]interface{}, 0)
+
+	v = append(
+		v,
+		utils.Checksum(c.ServiceId),
+		utils.Checksum(c.CustomvarId),
+		utils.Checksum(c.EnvId),
+	)
+
+	return v
+}
+
+func (c *ServiceCustomvar) GetId() string {
+	return c.Id
+}
+
+func (c *ServiceCustomvar) SetId(id string) {
+	c.Id = id
+}
+
+func init() {
+	name := "service_customvar"
+	ObjectInformation = configobject.ObjectInformation{
+		ObjectType: name,
+		RedisKey: "service:customvar",
+		Factory: NewServiceCustomvar,
+		HasChecksum: false,
+		BulkInsertStmt: connection.NewBulkInsertStmt(name, Fields),
+		BulkDeleteStmt: connection.NewBulkDeleteStmt(name),
+		BulkUpdateStmt: connection.NewBulkUpdateStmt(name, Fields),
+	}
+}
