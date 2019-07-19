583289280e1d00af79d3fa7616d944067405f9fb
Noah Hilverling <noah.hilverling@icinga.com>
2019-07-19 13:58:30 +0200

    Add NotificationStatesToBitMask() and NotificationTypesToBitMask()

--- utils/convert.go
+++ utils/convert.go
@@ -9,6 +9,25 @@ var (
 		true:  "y",
 		false: "n",
 	}
+	NotificationStates = map[string]int{
+		"OK": 		1,
+		"Warning":	2,
+		"Critical": 4,
+		"Unknown":	8,
+		"Up":		16,
+		"Down":		32,
+	}
+	NotificationTypes = map[string]int{
+		"DowntimeStart": 	1,
+		"DowntimeEnd":		2,
+		"DowntimeRemoved": 	4,
+		"Custom":			8,
+		"Acknowledgement":	16,
+		"Problem":			32,
+		"Recovery":			64,
+		"FlappingStart":	128,
+		"FlappingEnd":		256,
+	}
 )
 
 // Checksum converts a hex string to a byte array
@@ -24,4 +43,22 @@ func Checksum(s string) []byte {
 // DecodeChecksum coverts a byte array into a hex string
 func DecodeChecksum(c []byte) string {
 	return hex.EncodeToString(c)
+}
+
+// Converts an array of notification state strings into a bit mask
+func NotificationStatesToBitMask(states []string) int {
+	mask := 0
+	for _, s := range states {
+		mask += NotificationStates[s]
+	}
+	return mask
+}
+
+// Converts an array of notification type strings into a bit mask
+func NotificationTypesToBitMask(types []string) int {
+	mask := 0
+	for _, t := range types {
+		mask += NotificationTypes[t]
+	}
+	return mask
 }
