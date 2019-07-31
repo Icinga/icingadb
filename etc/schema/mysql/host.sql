ad0ad2021d9bb5e8a69b71499de14c4e57cd616b
Noah Hilverling <noah.hilverling@icinga.com>
2019-07-31 15:45:26 +0200

    Host/Service: Commandline should not be restricted to 255 chars

--- etc/schema/mysql/host.sql
+++ etc/schema/mysql/host.sql
@@ -120,7 +120,7 @@ CREATE TABLE host_state (
   output text DEFAULT NULL,
   long_output text DEFAULT NULL,
   performance_data text DEFAULT NULL,
-  check_commandline varchar(255) DEFAULT NULL,
+  check_commandline text DEFAULT NULL,
 
   is_problem enum('y', 'n') NOT NULL,
   is_handled enum('y', 'n') NOT NULL,
