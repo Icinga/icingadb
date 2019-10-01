74cde372f192a15e26efb629f85144f2770f9887
Alexander A. Klimov <alexander.klimov@icinga.com>
2019-10-01 17:30:36 +0200

    Schema: add host history details tables as in the IDO

--- etc/schema/mysql/host.sql
+++ etc/schema/mysql/host.sql
@@ -145,3 +145,94 @@ CREATE TABLE host_state (
 
   PRIMARY KEY (host_id)
 ) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE host_notification_history (
+  id binary(16) NOT NULL COMMENT 'UUID',
+  environment_id binary(20) NOT NULL COMMENT 'environment.id',
+  host_id binary(20) NOT NULL COMMENT 'host.id',
+  notification_id binary(20) NOT NULL COMMENT 'notification.id',
+
+  type smallint(3) unsigned NOT NULL,
+  send_time bigint(20) unsigned NOT NULL,
+  state tinyint(1) unsigned NOT NULL,
+  output text DEFAULT NULL,
+  long_output text DEFAULT NULL,
+  users_notified smallint(5) unsigned NOT NULL,
+
+  PRIMARY KEY (id)
+) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE host_state_history (
+  id binary(16) NOT NULL COMMENT 'UUID',
+  environment_id binary(20) NOT NULL COMMENT 'environment.id',
+  host_id binary(20) NOT NULL COMMENT 'host.id',
+
+  change_time bigint(20) unsigned NOT NULL,
+  state_type enum('hard', 'soft') NOT NULL,
+  soft_state tinyint(1) unsigned NOT NULL,
+  hard_state tinyint(1) unsigned NOT NULL,
+  attempt tinyint(1) unsigned NOT NULL,
+  last_soft_state tinyint(1) unsigned NOT NULL,
+  last_hard_state tinyint(1) unsigned NOT NULL,
+  output text DEFAULT NULL,
+  long_output text DEFAULT NULL,
+  max_check_attempts int(10) unsigned NOT NULL,
+
+  PRIMARY KEY (id)
+) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE host_downtime_history (
+  id binary(16) NOT NULL COMMENT 'UUID',
+  environment_id binary(20) NOT NULL COMMENT 'environment.id',
+  host_id binary(20) NOT NULL COMMENT 'host.id',
+  downtime_id binary(20) NOT NULL COMMENT 'host_downtime.id',
+  triggered_by_id binary(20) NOT NULL COMMENT 'host_downtime.id',
+
+  entry_time bigint(20) unsigned NOT NULL,
+  author varchar(255) NOT NULL COLLATE utf8mb4_unicode_ci,
+  comment text NOT NULL,
+  is_fixed enum('y', 'n') NOT NULL,
+  duration bigint(20) unsigned NOT NULL,
+  scheduled_start_time bigint(20) unsigned NOT NULL,
+  scheduled_end_time bigint(20) unsigned NOT NULL,
+  was_started enum('y', 'n') NOT NULL,
+  actual_start_time bigint(20) unsigned DEFAULT NULL COMMENT 'Time when the host went into a problem state during the downtimes timeframe',
+  actual_end_time bigint(20) unsigned DEFAULT NULL COMMENT 'Problem state assumed: scheduled_end_time if fixed, start_time + duration otherwise',
+  was_cancelled enum('y', 'n') NOT NULL,
+  is_in_effect enum('y', 'n') NOT NULL,
+  trigger_time bigint(20) unsigned NOT NULL,
+  deletion_time bigint(20) unsigned NOT NULL,
+
+  PRIMARY KEY (id)
+) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE host_comment_history (
+  id binary(16) NOT NULL COMMENT 'UUID',
+  environment_id binary(20) NOT NULL COMMENT 'environment.id',
+  host_id binary(20) NOT NULL COMMENT 'host.id',
+  comment_id binary(20) NOT NULL COMMENT 'host_comment.id',
+
+  entry_time bigint(20) unsigned NOT NULL,
+  author varchar(255) NOT NULL COLLATE utf8mb4_unicode_ci,
+  comment text NOT NULL,
+  entry_type enum('comment','ack','downtime','flapping') NOT NULL,
+  is_persistent enum('y','n') NOT NULL,
+  expire_time bigint(20) unsigned DEFAULT NULL,
+  deletion_time bigint(20) unsigned NOT NULL,
+
+  PRIMARY KEY (id)
+) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE host_flapping_history (
+  id binary(16) NOT NULL COMMENT 'UUID',
+  environment_id binary(20) NOT NULL COMMENT 'environment.id',
+  host_id binary(20) NOT NULL COMMENT 'host.id',
+
+  event_time bigint(20) unsigned NOT NULL,
+  event_type enum('start', 'end') NOT NULL,
+  percent_state_change float unsigned NOT NULL,
+  flapping_threshold_low float unsigned NOT NULL,
+  flapping_threshold_high float unsigned NOT NULL,
+
+  PRIMARY KEY (id)
+) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
