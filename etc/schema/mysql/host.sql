bedcabcab9e584440f1bf079ca78a8c36488c991
Alexander A. Klimov <alexander.klimov@icinga.com>
2019-10-02 15:45:20 +0200

    Schema: make *_*_history.{deletion_time,end_time} NULL DEFAULT NULL

--- etc/schema/mysql/host.sql
+++ etc/schema/mysql/host.sql
@@ -200,7 +200,7 @@ CREATE TABLE host_downtime_history (
   was_cancelled enum('y', 'n') NOT NULL,
   is_in_effect enum('y', 'n') NOT NULL,
   trigger_time bigint(20) unsigned NOT NULL,
-  deletion_time bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '0 = not deleted yet',
+  deletion_time bigint(20) unsigned NULL DEFAULT NULL,
 
   PRIMARY KEY (downtime_id)
 ) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
@@ -216,7 +216,7 @@ CREATE TABLE host_comment_history (
   entry_type enum('comment','ack','downtime','flapping') NOT NULL,
   is_persistent enum('y','n') NOT NULL,
   expire_time bigint(20) unsigned DEFAULT NULL,
-  deletion_time bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '0 = not deleted yet',
+  deletion_time bigint(20) unsigned NULL DEFAULT NULL,
 
   PRIMARY KEY (comment_id)
 ) ENGINE=InnoDb ROW_FORMAT=DYNAMIC DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
@@ -227,7 +227,7 @@ CREATE TABLE host_flapping_history (
   host_id binary(20) NOT NULL COMMENT 'host.id',
 
   start_time bigint(20) unsigned NOT NULL,
-  end_time bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT '0 = not ended yet',
+  end_time bigint(20) unsigned NULL DEFAULT NULL,
   percent_state_change float unsigned NOT NULL,
   flapping_threshold_low float unsigned NOT NULL,
   flapping_threshold_high float unsigned NOT NULL,
