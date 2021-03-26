474369387262469c915ad8a89982e2c6eb7ad138
Alexander A. Klimov <alexander.klimov@icinga.com>
2021-03-26 14:16:20 +0100

    Introduce types.CommentType

--- pkg/icingadb/v1/comment.go
+++ pkg/icingadb/v1/comment.go
@@ -9,17 +9,17 @@ type Comment struct {
 	EntityWithChecksum `json:",inline"`
 	EnvironmentMeta    `json:",inline"`
 	NameMeta           `json:",inline"`
-	ObjectType         string          `json:"object_type"`
-	HostId             types.Binary    `json:"host_id"`
-	ServiceId          types.Binary    `json:"service_id"`
-	Author             string          `json:"author"`
-	Text               string          `json:"text"`
-	EntryType          string          `json:"entry_type"`
-	EntryTime          types.UnixMilli `json:"entry_time"`
-	IsPersistent       types.Bool      `json:"is_persistent"`
-	IsSticky           types.Bool      `json:"is_sticky"`
-	ExpireTime         types.UnixMilli `json:"expire_time"`
-	ZoneId             types.Binary    `json:"zone_id"`
+	ObjectType         string            `json:"object_type"`
+	HostId             types.Binary      `json:"host_id"`
+	ServiceId          types.Binary      `json:"service_id"`
+	Author             string            `json:"author"`
+	Text               string            `json:"text"`
+	EntryType          types.CommentType `json:"entry_type"`
+	EntryTime          types.UnixMilli   `json:"entry_time"`
+	IsPersistent       types.Bool        `json:"is_persistent"`
+	IsSticky           types.Bool        `json:"is_sticky"`
+	ExpireTime         types.UnixMilli   `json:"expire_time"`
+	ZoneId             types.Binary      `json:"zone_id"`
 }
 
 func NewComment() contracts.Entity {
