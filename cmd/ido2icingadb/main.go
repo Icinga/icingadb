9a0f83845976c9724e7886cf3d5c719de444cd12
Alexander A. Klimov <alexander.klimov@icinga.com>
2021-08-03 13:30:41 +0200

    cmd/ido2icingadb: parse config file

--- cmd/ido2icingadb/main.go
+++ cmd/ido2icingadb/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"github.com/goccy/go-yaml"
 	"github.com/icinga/icingadb/cmd/internal"
+	"github.com/icinga/icingadb/pkg/config"
 	"github.com/jessevdk/go-flags"
 	"os"
 )
@@ -12,6 +15,12 @@ type Flags struct {
 	Config string `short:"c" long:"config" description:"path to config file" required:"true"`
 }
 
+// Config defines the YAML config structure.
+type Config struct {
+	IDO      config.Database `yaml:"ido"`
+	IcingaDB config.Database `yaml:"icingadb"`
+}
+
 func main() {
 	os.Exit(run())
 }
@@ -20,6 +29,24 @@ func run() int {
 	f := &Flags{}
 	if _, err := flags.NewParser(f, flags.Default).Parse(); err != nil {
 		return 2
+	}
+
+	cf, err := os.Open(f.Config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can't open config file: %s\n", err.Error())
+		return 2
+	}
+
+	c := &Config{}
+
+	{
+		err := yaml.NewDecoder(cf).Decode(c)
+		cf.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "can't parse config file: %s\n", err.Error())
+			return 2
+		}
 	}
 
 	// TODO
