acde6ade697c0b207b460cb92cd4584c4753c5ec
Ravi Kumar Kempapura Srinivasa <ravi.srinivasa@icinga.com>
2021-09-20 13:23:00 +0200

    Introduce Logging config struct

---
+++ pkg/config/logging.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"github.com/icinga/icingadb/internal/logging"
+	"go.uber.org/zap/zapcore"
+)
+
+// Logging defines Logger configuration.
+type Logging struct {
+	// zapcore.Level at 0 is for info level.
+	Level zapcore.Level `yaml:"level" default:"0"`
+
+	logging.Options `yaml:"options"`
+}
