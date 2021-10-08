8a1c217b15898b93a480133037bf2831756a51f6
Alexander A. Klimov <alexander.klimov@icinga.com>
2021-10-08 12:38:06 +0200

    cmd/ido2icingadb: document cache.go

--- cmd/ido2icingadb/cache.go
+++ cmd/ido2icingadb/cache.go
@@ -30,7 +30,7 @@ var eventTimeCacheSchema = []string{
 // Therefore: Stream IDO's icinga_flappinghistory once, compute flapping_history#start_time
 // and cache it into an SQLite database. Then steam from that database and the IDO.
 //
-// Similar for acknowledgements.
+// Similar for acknowledgements. (On non-recoverable errors the whole program exits.)
 func buildEventTimeCache(ht *historyType, idoColumns []string) {
 	chunkCacheTx(ht.cache, func(tx **sqlx.Tx, onDeleted func(sql.Result), onNewUncommittedDml func()) {
 		var checkpoint struct {
@@ -42,6 +42,7 @@ func buildEventTimeCache(ht *historyType, idoColumns []string) {
 		ht.bar.SetCurrent(checkpoint.Cnt * 2)
 		inc := barIncrementer{ht.bar, time.Now()}
 
+		// Stream source data...
 		sliceIdoHistory(
 			ht.snapshot,
 			"SELECT "+strings.Join(idoColumns, ", ")+" FROM "+ht.idoTable+
@@ -49,7 +50,7 @@ func buildEventTimeCache(ht *historyType, idoColumns []string) {
 				// so it makes no sense to take vanished objects into account.
 				" xh USE INDEX (PRIMARY) INNER JOIN icinga_objects o ON o.object_id=xh.object_id WHERE xh."+
 				ht.idoIdColumn+" > ? ORDER BY xh."+ht.idoIdColumn+" LIMIT ?",
-			nil, checkpoint.MaxId.Int64,
+			nil, checkpoint.MaxId.Int64, // ... since we were interrupted:
 			func(idoRows []struct {
 				Id            uint64
 				EventTime     int64
@@ -59,6 +60,7 @@ func buildEventTimeCache(ht *historyType, idoColumns []string) {
 			}) (checkpoint interface{}) {
 				for _, idoRow := range idoRows {
 					if idoRow.EventIsStart == 0 {
+						// Ack/flapping end event. Get the start event time:
 						var lst []struct {
 							EventTime     int64
 							EventTimeUsec uint32
@@ -68,22 +70,28 @@ func buildEventTimeCache(ht *historyType, idoColumns []string) {
 							idoRow.ObjectId,
 						)
 
+						// If we have that, ...
 						if len(lst) > 0 {
+							// ... save the start event time for the actual migration:
 							cacheExec(
 								*tx, false,
 								"INSERT INTO end_start_time(history_id, event_time, event_time_usec) VALUES (?, ?, ?)",
 								idoRow.Id, lst[0].EventTime, lst[0].EventTimeUsec,
 							)
 
+							// This previously queried info isn't needed anymore.
 							onDeleted(cacheExec(
 								*tx, false, "DELETE FROM last_start_time WHERE object_id=?", idoRow.ObjectId,
 							))
 						}
 					} else {
+						// Ack/flapping start event directly after another start event (per checkable).
+						// The old one won't have (but the new one will) an end event (which will need its time).
 						onDeleted(cacheExec(
 							*tx, false, "DELETE FROM last_start_time WHERE object_id=?", idoRow.ObjectId,
 						))
 
+						// An ack/flapping start event. The following end event (per checkable) will need its time.
 						cacheExec(
 							*tx, false,
 							"INSERT INTO last_start_time(object_id, event_time, event_time_usec) VALUES (?, ?, ?)",
@@ -100,6 +108,7 @@ func buildEventTimeCache(ht *historyType, idoColumns []string) {
 			},
 		)
 
+		// This never queried info isn't needed anymore.
 		onDeleted(cacheExec(*tx, false, "DELETE FROM last_start_time"))
 	})
 
@@ -133,7 +142,7 @@ var previousHardStateCacheSchema = []string{
 // Therefore: Stream IDO's icinga_statehistory once, compute state_history#previous_hard_state
 // and cache it into an SQLite database. Then steam from that database and the IDO.
 //
-// Similar for notifications.
+// Similar for notifications. (On non-recoverable errors the whole program exits.)
 func buildPreviousHardStateCache(ht *historyType, idoColumns []string) {
 	chunkCacheTx(ht.cache, func(tx **sqlx.Tx, onDeleted func(sql.Result), onNewUncommittedDml func()) {
 		var nextIds struct {
@@ -146,16 +155,16 @@ func buildPreviousHardStateCache(ht *historyType, idoColumns []string) {
 		cacheGet(*tx, &previousHardState, "SELECT COUNT(*) cnt FROM previous_hard_state")
 
 		var checkpoint int64
-		if nextIds.MinId.Valid {
-			checkpoint = nextIds.MinId.Int64
-		} else {
+		if nextIds.MinId.Valid { // there are next_ids
+			checkpoint = nextIds.MinId.Int64 // this kind of caches is filled descending
+		} else { // there aren't any next_ids
 			// next_ids contains the most recently processed IDs and is only empty if...
 			if previousHardState.Cnt == 0 {
 				// ... we didn't actually start yet...
-				checkpoint = (1 << 63) - 1
+				checkpoint = (1 << 63) - 1 // start from the largest (possible) ID
 			} else {
 				// ... or we've already finished.
-				checkpoint = 0
+				checkpoint = 0 // make following query no-op
 			}
 		}
 
@@ -169,6 +178,7 @@ func buildPreviousHardStateCache(ht *historyType, idoColumns []string) {
 		// 2. If the history gets cleaned up between two migration trials,
 		//    the difference either just doesn't appear in the cache or - if already there - will be ignored later.
 
+		// Stream source data...
 		sliceIdoHistory(
 			ht.snapshot,
 			"SELECT "+strings.Join(idoColumns, ", ")+" FROM "+ht.idoTable+
@@ -176,7 +186,7 @@ func buildPreviousHardStateCache(ht *historyType, idoColumns []string) {
 				// so it makes no sense to take vanished objects into account.
 				" xh USE INDEX (PRIMARY) INNER JOIN icinga_objects o ON o.object_id=xh.object_id WHERE xh."+
 				ht.idoIdColumn+" < ? ORDER BY xh."+ht.idoIdColumn+" DESC LIMIT ?",
-			nil, checkpoint,
+			nil, checkpoint, // ... since we were interrupted:
 			func(idoRows []struct {
 				Id            uint64
 				ObjectId      uint64
@@ -186,30 +196,39 @@ func buildPreviousHardStateCache(ht *historyType, idoColumns []string) {
 					var nhs []struct{ NextHardState uint8 }
 					cacheSelect(*tx, &nhs, "SELECT next_hard_state FROM next_hard_state WHERE object_id=?", idoRow.ObjectId)
 
-					if len(nhs) < 1 {
+					if len(nhs) < 1 { // we just started (per checkable)
+						// At the moment (we're "travelling back in time") that's the checkable's hard state:
 						cacheExec(
 							*tx, false, "INSERT INTO next_hard_state(object_id, next_hard_state) VALUES (?, ?)",
 							idoRow.ObjectId, idoRow.LastHardState,
 						)
 
+						// But for the current time point the previous hard state isn't known, yet:
 						cacheExec(
 							*tx, false, "INSERT INTO next_ids(history_id, object_id) VALUES (?, ?)",
 							idoRow.Id, idoRow.ObjectId,
 						)
 					} else if idoRow.LastHardState == nhs[0].NextHardState {
+						// The hard state didn't change yet (per checkable),
+						// so this time point also awaits the previous hard state.
 						cacheExec(
 							*tx, false, "INSERT INTO next_ids(history_id, object_id) VALUES (?, ?)",
 							idoRow.Id, idoRow.ObjectId,
 						)
-					} else {
+					} else { // the hard state changed (per checkable)
+						// That past hard state is now available for the processed future time points:
 						cacheExec(
 							*tx, false, "INSERT INTO previous_hard_state(history_id, previous_hard_state) "+
 								"SELECT history_id, ? FROM next_ids WHERE object_id=?",
 							idoRow.LastHardState, idoRow.ObjectId,
 						)
 
+						// Now they have what they wanted:
 						onDeleted(cacheExec(*tx, false, "DELETE FROM next_hard_state WHERE object_id=?", idoRow.ObjectId))
 						onDeleted(cacheExec(*tx, false, "DELETE FROM next_ids WHERE object_id=?", idoRow.ObjectId))
+
+						// That's done.
+						// Now do the same thing as in the "we just started" case above, for the same reason:
 
 						cacheExec(
 							*tx, false, "INSERT INTO next_hard_state(object_id, next_hard_state) VALUES (?, ?)",
@@ -231,11 +250,13 @@ func buildPreviousHardStateCache(ht *historyType, idoColumns []string) {
 			},
 		)
 
+		// No past hard state is available for the processed future time points, assuming pending:
 		cacheExec(
 			*tx, false, "INSERT INTO previous_hard_state(history_id, previous_hard_state) "+
 				"SELECT history_id, 99 FROM next_ids",
 		)
 
+		// Now they should have what they wanted:
 		onDeleted(cacheExec(*tx, false, "DELETE FROM next_hard_state"))
 		onDeleted(cacheExec(*tx, false, "DELETE FROM next_ids"))
 	})
@@ -243,10 +264,14 @@ func buildPreviousHardStateCache(ht *historyType, idoColumns []string) {
 	ht.bar.SetTotal(ht.bar.Current(), true)
 }
 
+// chunkCacheTx rationale: during do operate on cache via *tx. On every completed operation call onNewUncommittedDml()
+// which periodically commits *tx and starts a new tx. (That's why tx is a **, not just a *.) On every DELETE
+// call onDeleted() which will cause a VACUUM after do if any rows were affected to save some space.
+// (On non-recoverable errors the whole program exits.)
 func chunkCacheTx(cache *sqlx.DB, do func(tx **sqlx.Tx, onDeleted func(sql.Result), onNewUncommittedDml func())) {
 	logger := log.With("backend", "cache")
-	var deleted int64
-	var inTx int
+	var totalAffectedByDeletes int64
+	var onNewUncommittedDmlCallsSinceLastTx int
 
 	tx, err := cache.Beginx()
 	if err != nil {
@@ -255,16 +280,16 @@ func chunkCacheTx(cache *sqlx.DB, do func(tx **sqlx.Tx, onDeleted func(sql.Resul
 
 	do(
 		&tx,
-		func(result sql.Result) {
+		func(result sql.Result) { // onDeleted
 			if affected, err := result.RowsAffected(); err == nil {
-				deleted += affected
+				totalAffectedByDeletes += affected
 			} else {
 				log.Errorf("%+v", errors.Wrap(err, "can't get affected rows"))
 			}
 		},
-		func() {
-			inTx++
-			if inTx == bulk {
+		func() { // onNewUncommittedDml
+			onNewUncommittedDmlCallsSinceLastTx++
+			if onNewUncommittedDmlCallsSinceLastTx == bulk {
 				if err := tx.Commit(); err != nil {
 					logger.Fatalf("%+v", errors.Wrap(err, "can't commit transaction"))
 				}
@@ -276,7 +301,7 @@ func chunkCacheTx(cache *sqlx.DB, do func(tx **sqlx.Tx, onDeleted func(sql.Resul
 					logger.Fatalf("%+v", errors.Wrap(err, "can't begin transaction"))
 				}
 
-				inTx = 0
+				onNewUncommittedDmlCallsSinceLastTx = 0
 			}
 		},
 	)
@@ -285,11 +310,12 @@ func chunkCacheTx(cache *sqlx.DB, do func(tx **sqlx.Tx, onDeleted func(sql.Resul
 		logger.Fatalf("%+v", errors.Wrap(err, "can't commit transaction"))
 	}
 
-	if deleted > 0 {
+	if totalAffectedByDeletes > 0 {
 		cacheExec(cache, true, "VACUUM")
 	}
 }
 
+// cacheGet does cache.Get(dest, query, args...). (On non-recoverable errors the whole program exits.)
 func cacheGet(cache interface {
 	Get(dest interface{}, query string, args ...interface{}) error
 }, dest interface{}, query string, args ...interface{}) {
@@ -299,6 +325,7 @@ func cacheGet(cache interface {
 	}
 }
 
+// cacheSelect does cacheTx.Select(dest, query, args...). (On non-recoverable errors the whole program exits.)
 func cacheSelect(cacheTx *sqlx.Tx, dest interface{}, query string, args ...interface{}) {
 	if err := cacheTx.Select(dest, query, args...); err != nil {
 		log.With("backend", "cache", "query", query, "args", args).
@@ -306,6 +333,7 @@ func cacheSelect(cacheTx *sqlx.Tx, dest interface{}, query string, args ...inter
 	}
 }
 
+// cacheExec does cache.Exec(dml, args...). On non-recoverable errors the whole program exits if !allowFailure.
 func cacheExec(cache sqlx.Execer, allowFailure bool, dml string, args ...interface{}) sql.Result {
 	res, err := cache.Exec(dml, args...)
 	if err != nil {
