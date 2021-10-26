12525c78726e8951e864b279e36398994484ed09
Eric Lippmann <eric.lippmann@icinga.com>
2021-10-26 14:50:39 +0200

    Use pkg periodic for overdue sync logs

--- pkg/icingadb/overdue/sync.go
+++ pkg/icingadb/overdue/sync.go
@@ -6,16 +6,17 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
 	"github.com/icinga/icingadb/internal"
+	"github.com/icinga/icingadb/pkg/com"
 	"github.com/icinga/icingadb/pkg/contracts"
 	"github.com/icinga/icingadb/pkg/icingadb"
 	"github.com/icinga/icingadb/pkg/icingadb/v1"
 	"github.com/icinga/icingadb/pkg/icingadb/v1/overdue"
 	"github.com/icinga/icingadb/pkg/icingaredis"
+	"github.com/icinga/icingadb/pkg/periodic"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 	"strconv"
-	"sync/atomic"
 	"time"
 )
 
@@ -57,23 +58,19 @@ func (s Sync) Sync(ctx context.Context) error {
 	}
 
 	g, ctx := errgroup.WithContext(ctx)
-	hostCounter := new(uint64)
-	serviceCounter := new(uint64)
+
+	var hostCounter com.Counter
+	defer s.log(ctx, "host", &hostCounter).Stop()
+
+	var serviceCounter com.Counter
+	defer s.log(ctx, "service", &serviceCounter).Stop()
 
 	g.Go(func() error {
-		return s.sync(ctx, "host", overdue.NewHostState, hostCounter)
+		return s.sync(ctx, "host", overdue.NewHostState, &hostCounter)
 	})
 
 	g.Go(func() error {
-		return s.log(ctx, "host", hostCounter)
-	})
-
-	g.Go(func() error {
-		return s.sync(ctx, "service", overdue.NewServiceState, serviceCounter)
-	})
-
-	g.Go(func() error {
-		return s.log(ctx, "service", serviceCounter)
+		return s.sync(ctx, "service", overdue.NewServiceState, &serviceCounter)
 	})
 
 	return g.Wait()
@@ -124,21 +121,12 @@ func (s Sync) initSync(ctx context.Context, objectType string) error {
 }
 
 // log periodically logs sync's workload.
-func (s Sync) log(ctx context.Context, objectType string, counter *uint64) error {
-	const period = 20 * time.Second
-	periodically := time.NewTicker(period)
-	defer periodically.Stop()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-periodically.C:
-			if count := atomic.SwapUint64(counter, 0); count > 0 {
-				s.logger.Infof("Synced %d %s overdue indicators in the last %s", count, objectType, period)
-			}
+func (s Sync) log(ctx context.Context, objectType string, counter *com.Counter) periodic.Stoper {
+	return periodic.Start(ctx, internal.LoggingInterval(), func(_ periodic.Tick) {
+		if count := counter.Reset(); count > 0 {
+			s.logger.Infof("Synced %d %s overdue indicators", count, objectType)
 		}
-	}
+	})
 }
 
 // luaGetOverdues takes the following KEYS:
@@ -176,7 +164,7 @@ return res
 `)
 
 // sync synchronizes Redis overdue sets from s.redis to s.db for objectType.
-func (s Sync) sync(ctx context.Context, objectType string, factory factory, counter *uint64) error {
+func (s Sync) sync(ctx context.Context, objectType string, factory factory, counter *com.Counter) error {
 	s.logger.Infof("Syncing %s overdue indicators", objectType)
 
 	keys := [3]string{"icinga:nextupdate:" + objectType, "icingadb:overdue:" + objectType, ""}
@@ -231,7 +219,7 @@ func (s Sync) sync(ctx context.Context, objectType string, factory factory, coun
 // updateOverdue sets objectType_state#is_overdue for ids to overdue
 // and updates icingadb:overdue:objectType respectively.
 func (s Sync) updateOverdue(
-	ctx context.Context, objectType string, factory factory, counter *uint64, ids []interface{}, overdue bool,
+	ctx context.Context, objectType string, factory factory, counter *com.Counter, ids []interface{}, overdue bool,
 ) error {
 	if len(ids) < 1 {
 		return nil
@@ -241,7 +229,7 @@ func (s Sync) updateOverdue(
 		return errors.Wrap(err, "can't update overdue indicators")
 	}
 
-	atomic.AddUint64(counter, uint64(len(ids)))
+	counter.Add(uint64(len(ids)))
 
 	var op func(ctx context.Context, key string, members ...interface{}) *redis.IntCmd
 	if overdue {
