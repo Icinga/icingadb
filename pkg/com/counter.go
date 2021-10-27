a6e02e7f3c167084d704224c5bb281c286427d3e
Eric Lippmann <eric.lippmann@icinga.com>
2021-10-27 10:36:04 +0200

    Introduce Counter.Total()

--- pkg/com/counter.go
+++ pkg/com/counter.go
@@ -1,13 +1,20 @@
 package com
 
-import "sync/atomic"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // Counter implements an atomic counter.
-type Counter uint64
+type Counter struct {
+	value uint64
+	mu    sync.Mutex // Protects total.
+	total uint64
+}
 
 // Add adds the given delta to the counter.
 func (c *Counter) Add(delta uint64) {
-	atomic.AddUint64(c.ptr(), delta)
+	atomic.AddUint64(&c.value, delta)
 }
 
 // Inc increments the counter by one.
@@ -16,15 +23,26 @@ func (c *Counter) Inc() {
 }
 
 // Reset resets the counter to 0 and returns its previous value.
+// Does not reset the total value returned from Total.
 func (c *Counter) Reset() uint64 {
-	return atomic.SwapUint64(c.ptr(), 0)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	v := atomic.SwapUint64(&c.value, 0)
+	c.total += v
+
+	return v
 }
 
-// Val returns the counter value.
+// Total returns the total counter value.
+func (c *Counter) Total() uint64 {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.total + c.Val()
+}
+
+// Val returns the current counter value.
 func (c *Counter) Val() uint64 {
-	return atomic.LoadUint64(c.ptr())
-}
-
-func (c *Counter) ptr() *uint64 {
-	return (*uint64)(c)
+	return atomic.LoadUint64(&c.value)
 }
