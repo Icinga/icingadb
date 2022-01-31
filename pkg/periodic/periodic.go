17a7b026ce1ba11a09cded431655e62306c1e33e
Eric Lippmann <eric.lippmann@icinga.com>
2022-01-31 19:46:55 +0100

    Add immediate option for periodic tasks

--- pkg/periodic/periodic.go
+++ pkg/periodic/periodic.go
@@ -25,6 +25,13 @@ type Tick struct {
 	Time    time.Time
 }
 
+// Immediate starts the periodic task immediately instead of after the first tick.
+func Immediate() Option {
+	return optionFunc(func(p *periodic) {
+		p.immediate = true
+	})
+}
+
 // OnStop configures a callback that is executed when a periodic task is stopped or canceled.
 func OnStop(f func(Tick)) Option {
 	return optionFunc(func(p *periodic) {
@@ -34,6 +41,8 @@ func OnStop(f func(Tick)) Option {
 
 // Start starts a periodic task with a ticker at the specified interval,
 // which executes the given callback after each tick.
+// Pending tasks do not overlap, but could start immediately if
+// the previous task(s) takes longer than the interval.
 // Call Stop() on the return value in order to stop the ticker and to release associated resources.
 // The interval must be greater than zero.
 func Start(ctx context.Context, interval time.Duration, callback func(Tick), options ...Option) Stopper {
@@ -47,30 +56,44 @@ func Start(ctx context.Context, interval time.Duration, callback func(Tick), opt
 	}
 
 	ctx, cancelCtx := context.WithCancel(ctx)
-	ticker := time.NewTicker(t.interval)
+
 	start := time.Now()
 
 	go func() {
-		defer ticker.Stop()
+		done := false
 
-		for {
+		if !t.immediate {
 			select {
-			case tickTime := <-ticker.C:
+			case <-time.After(interval):
+			case <-ctx.Done():
+				done = true
+			}
+		}
+
+		if !done {
+			ticker := time.NewTicker(t.interval)
+			defer ticker.Stop()
+
+			for tickTime := time.Now(); !done; {
 				t.callback(Tick{
 					Elapsed: tickTime.Sub(start),
 					Time:    tickTime,
 				})
-			case <-ctx.Done():
-				if t.onStop != nil {
-					now := time.Now()
-					t.onStop(Tick{
-						Elapsed: now.Sub(start),
-						Time:    now,
-					})
-				}
 
-				return
+				select {
+				case tickTime = <-ticker.C:
+				case <-ctx.Done():
+					done = true
+				}
 			}
+		}
+
+		if t.onStop != nil {
+			now := time.Now()
+			t.onStop(Tick{
+				Elapsed: now.Sub(start),
+				Time:    now,
+			})
 		}
 	}()
 
@@ -92,8 +115,9 @@ func (f stoperFunc) Stop() {
 }
 
 type periodic struct {
-	interval time.Duration
-	callback func(Tick)
-	stop     sync.Once
-	onStop   func(Tick)
+	interval  time.Duration
+	callback  func(Tick)
+	immediate bool
+	stop      sync.Once
+	onStop    func(Tick)
 }
