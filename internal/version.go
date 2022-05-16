c6e843146f54fb7bec3386b80ec6d231e235cd63
Julian Brost <julian.brost@icinga.com>
2022-05-16 17:47:39 +0200

    Add more information to `icingadb --version` ouput
    This commit adds the build information (Go version, Git commit) and system
    information (distribution with version) to the output of `icingadb --version`.
    
    When exporting the source code using `git archive`, Git will insert the version
    information using the export-subst attribute (adds both a pretty version string
    like `git describe` and the commit hash).
    
    `go build` also adds some Git commit information to the resulting binary when
    building from a Git working directory (only the commit hash and a flag if there
    are uncommitted changes, but no pretty version string).
    
    Note that `go build` does not include Git version information in the binary, so
    when running directly from a Git working directory, commit information won't be
    available in the binary, but when doing this, you should be well aware which
    version you're running anyways.
    
    System information is obtained from the `os-release` file.

--- internal/version.go
+++ internal/version.go
@@ -1,3 +1,10 @@
 package internal
 
-var Version = "v1.0.0-rc2-dev"
+import (
+	"github.com/icinga/icingadb/pkg/version"
+)
+
+// Version contains version and Git commit information.
+//
+// The placeholders are replaced on `git archive` using the `export-subst` attribute.
+var Version = version.Version("1.0.0-rc2", "$Format:%(describe)$", "$Format:%H$")
