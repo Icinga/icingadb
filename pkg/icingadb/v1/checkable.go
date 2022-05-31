154f46c2f0480a954a11d24e01afb5ae6f8d2005
Yonas Habteab <yonas.habteab@icinga.com>
2022-05-31 15:12:37 +0200

    Checkable: Adjust the naming of the struct/json keys according to the new scheme

--- pkg/icingadb/v1/checkable.go
+++ pkg/icingadb/v1/checkable.go
@@ -12,17 +12,17 @@ type Checkable struct {
 	ActionUrlId           types.Binary `json:"action_url_id"`
 	ActiveChecksEnabled   types.Bool   `json:"active_checks_enabled"`
 	CheckInterval         float64      `json:"check_interval"`
-	CheckTimeperiod       string       `json:"check_timeperiod"`
+	CheckTimeperiodName   string       `json:"check_timeperiod_name"`
 	CheckTimeperiodId     types.Binary `json:"check_timeperiod_id"`
 	CheckRetryInterval    float64      `json:"check_retry_interval"`
 	CheckTimeout          float64      `json:"check_timeout"`
-	Checkcommand          string       `json:"checkcommand"`
+	CheckcommandName      string       `json:"checkcommand_name"`
 	CheckcommandId        types.Binary `json:"checkcommand_id"`
-	CommandEndpoint       string       `json:"command_endpoint"`
+	CommandEndpointName   string       `json:"command_endpoint_name"`
 	CommandEndpointId     types.Binary `json:"command_endpoint_id"`
 	DisplayName           string       `json:"display_name"`
 	EventHandlerEnabled   types.Bool   `json:"event_handler_enabled"`
-	Eventcommand          string       `json:"eventcommand"`
+	EventcommandName      string       `json:"eventcommand_name"`
 	EventcommandId        types.Binary `json:"eventcommand_id"`
 	FlappingEnabled       types.Bool   `json:"flapping_enabled"`
 	FlappingThresholdHigh float64      `json:"flapping_threshold_high"`
@@ -36,7 +36,7 @@ type Checkable struct {
 	NotificationsEnabled  types.Bool   `json:"notifications_enabled"`
 	PassiveChecksEnabled  types.Bool   `json:"passive_checks_enabled"`
 	PerfdataEnabled       types.Bool   `json:"perfdata_enabled"`
-	Zone                  string       `json:"zone"`
+	ZoneName              string       `json:"zone_name"`
 	ZoneId                types.Binary `json:"zone_id"`
 }
 
