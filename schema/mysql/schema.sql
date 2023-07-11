f46193a3729a61f62fac9da947b1e236e0c4fd1b
Johannes Meyer <johannes.meyer@icinga.com>
2023-07-11 09:27:08 +0200

    schema/mysql: Add missing indices to `hostgroup` and `servicegroup`
    fixes #615

--- schema/mysql/schema.sql
+++ schema/mysql/schema.sql
@@ -240,7 +240,9 @@ CREATE TABLE hostgroup (
 
   PRIMARY KEY (id),
 
-  INDEX idx_hostgroup_name (name) COMMENT 'Host/service/host group list filtered by host group name'
+  INDEX idx_hostgroup_display_name (display_name) COMMENT 'Hostgroup list filtered/ordered by display_name',
+  INDEX idx_hostgroup_name_ci (name_ci) COMMENT 'Hostgroup list filtered using quick search',
+  INDEX idx_hostgroup_name (name) COMMENT 'Host/service/host group list filtered by host group name; Hostgroup detail filter'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE hostgroup_member (
@@ -404,7 +406,9 @@ CREATE TABLE servicegroup (
 
   PRIMARY KEY (id),
 
-  INDEX idx_servicegroup_name (name) COMMENT 'Host/service/service group list filtered by service group name'
+  INDEX idx_servicegroup_display_name (display_name) COMMENT 'Servicegroup list filtered/ordered by display_name',
+  INDEX idx_servicegroup_name_ci (name_ci) COMMENT 'Servicegroup list filtered using quick search',
+  INDEX idx_servicegroup_name (name) COMMENT 'Host/service/service group list filtered by service group name; Servicegroup detail filter'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE servicegroup_member (
