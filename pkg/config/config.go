d7f6da797ac91210385ba19473ee77dbea5fafc9
Eric Lippmann <eric.lippmann@icinga.com>
2023-10-09 12:38:04 +0200

    Move `internal/config#FromYAMLFile()` to `config#FromYAMLFile()`

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,10 +3,46 @@ package config
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"github.com/creasty/defaults"
+	"github.com/goccy/go-yaml"
 	"github.com/jessevdk/go-flags"
 	"github.com/pkg/errors"
 	"io/ioutil"
+	"os"
 )
+
+type Validator interface {
+	Validate() error
+}
+
+// FromYAMLFile returns a new value of type T created from the given YAML config file.
+func FromYAMLFile[T any, P interface {
+	*T
+	Validator
+}](name string) (*T, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't open YAML file "+name)
+	}
+	defer f.Close()
+
+	c := P(new(T))
+
+	if err := defaults.Set(c); err != nil {
+		return nil, errors.Wrap(err, "can't set config defaults")
+	}
+
+	d := yaml.NewDecoder(f, yaml.DisallowUnknownField())
+	if err := d.Decode(c); err != nil {
+		return nil, errors.Wrap(err, "can't parse YAML file "+name)
+	}
+
+	if err := c.Validate(); err != nil {
+		return nil, errors.Wrap(err, "invalid configuration")
+	}
+
+	return c, nil
+}
 
 // ParseFlags parses CLI flags and
 // returns a Flags value created from them.
