82fffb4b9c13239861734385cc82e2e98b598bb3
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-09-04 12:21:20 +0200

    Test icingaredis/v1.StatsMessage#Time()

--- pkg/icingaredis/v1/stats_message_test.go
+++ pkg/icingaredis/v1/stats_message_test.go
@@ -1,8 +1,10 @@
 package v1
 
 import (
+	"github.com/icinga/icinga-go-library/types"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func TestStatsMessage_IcingaStatus(t *testing.T) {
@@ -61,6 +63,69 @@ func TestStatsMessage_IcingaStatus(t *testing.T) {
 	for _, st := range subtests {
 		t.Run(st.name, func(t *testing.T) {
 			if output, err := st.input.IcingaStatus(); st.error {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, &st.output, output)
+			}
+		})
+	}
+}
+
+func TestStatsMessage_Time(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  StatsMessage
+		output types.UnixMilli
+		error  bool
+	}{
+		{
+			name:  "nil",
+			error: true,
+		},
+		{
+			name:  "empty",
+			error: true,
+		},
+		{
+			name:  "timestampNil",
+			input: StatsMessage{"timestamp": nil},
+			error: true,
+		},
+		{
+			name:  "timestampBool",
+			input: StatsMessage{"timestamp": true},
+			error: true,
+		},
+		{
+			name:  "timestampInt",
+			input: StatsMessage{"timestamp": 42},
+			error: true,
+		},
+		{
+			name:  "timestampNotJSON",
+			input: StatsMessage{"timestamp": "not JSON"},
+			error: true,
+		},
+		{
+			name:  "timestampArray",
+			input: StatsMessage{"timestamp": "[]"},
+			error: true,
+		},
+		{
+			name:  "timestampNull",
+			input: StatsMessage{"timestamp": "null"},
+		},
+		{
+			name:   "timestamp",
+			input:  StatsMessage{"timestamp": "42023"},
+			output: types.UnixMilli(time.Unix(42, 23000000)),
+		},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			if output, err := st.input.Time(); st.error {
 				require.Error(t, err)
 			} else {
 				require.NoError(t, err)
