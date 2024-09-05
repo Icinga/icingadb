fb867dfac4afa998b555b08f27e5b91353fec5c6
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-09-05 17:27:49 +0200

    Test types.CommentType#Value()

--- pkg/icingadb/types/comment_type_test.go
+++ pkg/icingadb/types/comment_type_test.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"database/sql/driver"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -30,6 +31,30 @@ func TestCommentType_UnmarshalJSON(t *testing.T) {
 			} else {
 				require.NoError(t, err)
 				require.Equal(t, st.output, c)
+			}
+		})
+	}
+}
+
+func TestCommentType_Value(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  CommentType
+		output driver.Value
+		error  bool
+	}{
+		{name: "invalid", input: 5, error: true},
+		{name: "comment", input: 1, output: "comment"},
+		{name: "ack", input: 4, output: "ack"},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			if v, err := st.input.Value(); st.error {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, st.output, v)
 			}
 		})
 	}
