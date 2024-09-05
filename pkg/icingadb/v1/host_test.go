eda63668518119693320e653c07c20a2e4845c23
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-09-05 10:51:52 +0200

    Test icingadb/v1.Address6Bin#Value()

--- pkg/icingadb/v1/host_test.go
+++ pkg/icingadb/v1/host_test.go
@@ -28,3 +28,28 @@ func TestAddressBin_Value(t *testing.T) {
 		})
 	}
 }
+
+func TestAddress6Bin_Value(t *testing.T) {
+	ffff192020 := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 192, 0, 2, 0}
+
+	subtests := []struct {
+		name   string
+		input  *Host
+		output driver.Value
+	}{
+		{name: "nil-host"},
+		{name: "invalid-address", input: &Host{Address: "invalid"}},
+		{"IPv6", &Host{Address6: "2001:db8::"}, []byte{32, 1, 13, 184, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		{name: "IPv4", input: &Host{Address6: "192.0.2.0"}, output: ffff192020},
+		{name: "ffff-IPv4", input: &Host{Address6: "::ffff:192.0.2.0"}, output: ffff192020},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			v, err := (Address6Bin{Host: st.input}).Value()
+
+			require.NoError(t, err)
+			require.Equal(t, st.output, v)
+		})
+	}
+}
