6c7bf250930a9fd273f2e99c503de6bd8b4a7925
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-09-05 10:10:45 +0200

    Test icingadb.EntitiesById#IDs()

--- pkg/icingadb/entitiesbyid_test.go
+++ pkg/icingadb/entitiesbyid_test.go
@@ -1,6 +1,7 @@
 package icingadb
 
 import (
+	"github.com/icinga/icinga-go-library/types"
 	"github.com/icinga/icingadb/pkg/icingadb/v1"
 	"github.com/stretchr/testify/require"
 	"testing"
@@ -32,6 +33,40 @@ func TestEntitiesById_Keys(t *testing.T) {
 	for _, st := range subtests {
 		t.Run(st.name, func(t *testing.T) {
 			require.ElementsMatch(t, st.output, st.input.Keys())
+		})
+	}
+}
+
+func newEntity(id []byte) *v1.EntityWithoutChecksum {
+	return &v1.EntityWithoutChecksum{IdMeta: v1.IdMeta{Id: id}}
+}
+
+func TestEntitiesById_IDs(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  EntitiesById
+		output []types.Binary
+	}{
+		{name: "nil"},
+		{
+			name:  "empty",
+			input: EntitiesById{},
+		},
+		{
+			name:   "one",
+			input:  EntitiesById{"one": newEntity([]byte{23})},
+			output: []types.Binary{{23}},
+		},
+		{
+			name:   "two",
+			input:  EntitiesById{"one": newEntity([]byte{23}), "two": newEntity([]byte{42})},
+			output: []types.Binary{{23}, {42}},
+		},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			require.ElementsMatch(t, st.output, st.input.IDs())
 		})
 	}
 }
