31b757864f3217768170862c51057848aa1ca7ed
Yonas Habteab <yonas.habteab@icinga.com>
2024-10-24 17:03:23 +0200

    Write `create` & `delete` sla events on config dumps

--- pkg/icingadb/sla_lifecycle.go
+++ pkg/icingadb/sla_lifecycle.go
@@ -2,13 +2,19 @@ package icingadb
 
 import (
 	"context"
+	"fmt"
+	"github.com/icinga/icinga-go-library/backoff"
 	"github.com/icinga/icinga-go-library/database"
+	"github.com/icinga/icinga-go-library/retry"
 	"github.com/icinga/icinga-go-library/types"
 	v1 "github.com/icinga/icingadb/pkg/icingadb/v1"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 	"time"
 )
+
+// slaLifecycleTable defines the table name of v1.SlaLifecycle type.
+var slaLifecycleTable = database.TableName(v1.NewSlaLifecycle())
 
 // CreateSlaLifecyclesFromCheckables transforms the given checkables to sla lifecycle struct
 // and streams them into a returned channel.
@@ -67,4 +73,51 @@ func CreateSlaLifecyclesFromCheckables(
 	})
 
 	return slaLifecycles
+}
+
+// SyncCheckablesSlaLifecycle inserts one `create_time` sla lifecycle entry for each of the checkables from
+// the `host` and `service` tables and updates the `delete_time` of each of the sla lifecycle entries whose
+// host/service IDs cannot be found in the `host/service` tables.
+//
+// It's unlikely, but when a given Checkable doesn't already have a `create_time` entry in the database, the update
+// query won't update anything. Likewise, the insert statements may also become a no-op if the Checkables already
+// have a `create_time` entry with ´delete_time = 0`.
+//
+// This function retries any database errors for at least `5m` before giving up and failing with an error.
+func SyncCheckablesSlaLifecycle(ctx context.Context, db *database.DB) error {
+	hostInsertStmtFmt := `
+INSERT INTO %[1]s (id, environment_id, host_id, create_time)
+  SELECT id, environment_id, id, %[2]d AS create_time
+  FROM host WHERE NOT EXISTS(SELECT 1 FROM %[1]s WHERE service_id IS NULL AND delete_time = 0 AND host_id = host.id)`
+
+	hostUpdateStmtFmt := `
+UPDATE %[1]s SET delete_time = %[2]d
+  WHERE service_id IS NULL AND delete_time = 0 AND NOT EXISTS(SELECT 1 FROM host WHERE host.id = %[1]s.id)`
+
+	serviceInsertStmtFmt := `
+INSERT INTO %[1]s (id, environment_id, host_id, service_id, create_time)
+  SELECT id, environment_id, host_id, id, %[2]d AS create_time
+  FROM service WHERE NOT EXISTS(SELECT 1 FROM %[1]s WHERE delete_time = 0 AND service_id = service.id)`
+
+	serviceUpdateStmtFmt := `
+UPDATE %[1]s SET delete_time = %[2]d
+  WHERE delete_time = 0 AND service_id IS NOT NULL AND NOT EXISTS(SELECT 1 FROM service WHERE service.id = %[1]s.id)`
+
+	return retry.WithBackoff(
+		ctx,
+		func(context.Context) error {
+			eventTime := time.Now().UnixMilli()
+			for _, queryFmt := range []string{hostInsertStmtFmt, hostUpdateStmtFmt, serviceInsertStmtFmt, serviceUpdateStmtFmt} {
+				query := fmt.Sprintf(queryFmt, slaLifecycleTable, eventTime)
+				if _, err := db.ExecContext(ctx, query); err != nil {
+					return database.CantPerformQuery(err, query)
+				}
+			}
+
+			return nil
+		},
+		retry.Retryable,
+		backoff.NewExponentialWithJitter(1*time.Millisecond, 1*time.Second),
+		db.GetDefaultRetrySettings(),
+	)
 }
--- schema/mysql/upgrades/1.3.0.sql
+++ schema/mysql/upgrades/1.3.0.sql
@@ -18,5 +18,91 @@ ALTER TABLE checkcommand_envvar MODIFY COLUMN envvar_key varchar(255) NOT NULL;
 ALTER TABLE eventcommand_envvar MODIFY COLUMN envvar_key varchar(255) NOT NULL;
 ALTER TABLE notificationcommand_envvar MODIFY COLUMN envvar_key varchar(255) NOT NULL;
 
+CREATE TABLE sla_lifecycle (
+  id binary(20) NOT NULL COMMENT 'host.id if service_id is NULL otherwise service.id',
+  environment_id binary(20) NOT NULL COMMENT 'environment.id',
+  host_id binary(20) NOT NULL COMMENT 'host.id (may reference already deleted hosts)',
+  service_id binary(20) DEFAULT NULL COMMENT 'service.id (may reference already deleted services)',
+
+  -- These columns are nullable, but as we're using the delete_time to build the composed primary key, we have to set
+  -- this to `0` instead, since it's not allowed to use a nullable column as part of the primary key.
+  create_time bigint unsigned NOT NULL DEFAULT 0 COMMENT 'unix timestamp the event occurred',
+  delete_time bigint unsigned NOT NULL DEFAULT 0 COMMENT 'unix timestamp the delete event occurred',
+
+  PRIMARY KEY (id, delete_time)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+-- Insert a sla lifecycle create_time entry for all existing hosts with the LEAST timestamp found in either
+-- the sla_history_state or sla_history_downtime table, otherwise fallback to the current Unix timestamp.
+INSERT INTO sla_lifecycle (id, environment_id, host_id, create_time)
+  SELECT host.id,
+    host.environment_id,
+    host.id,
+    -- In MySQL/MariaDB, LEAST() returns NULL if either event_time or downtime_start is NULL, which is not
+    -- desirable for our use cases. So we need to work around this behaviour by nesting some COALESCE() calls.
+    COALESCE(LEAST(COALESCE(MIN(event_time), MIN(downtime_start)), COALESCE(MIN(downtime_start), MIN(event_time))), UNIX_TIMESTAMP() * 1000) AS create_time
+  FROM host
+    LEFT JOIN sla_history_state shs on host.id = shs.host_id AND shs.service_id IS NULL
+    LEFT JOIN sla_history_downtime shd on host.id = shd.host_id AND shd.service_id IS NULL
+  GROUP BY host.id, host.environment_id
+  ON DUPLICATE KEY UPDATE sla_lifecycle.id = sla_lifecycle.id;
+
+-- Insert a sla lifecycle deleted entry for all not existing hosts with the GREATEST timestamp
+-- found in either the sla_history_state or sla_history_downtime table.
+INSERT INTO sla_lifecycle (id, environment_id, host_id, delete_time)
+  SELECT host_id AS id,
+    environment_id,
+    host_id,
+    MAX(event_time) AS delete_time
+  FROM (SELECT host_id, environment_id, MAX(event_time) AS event_time
+    FROM sla_history_state
+      WHERE service_id IS NULL AND NOT EXISTS(SELECT 1 FROM host WHERE id = host_id)
+    GROUP BY host_id, environment_id
+    UNION ALL
+    SELECT host_id, environment_id, MAX(downtime_end) AS event_time
+    FROM sla_history_downtime
+      WHERE service_id IS NULL AND NOT EXISTS(SELECT 1 FROM host WHERE id = host_id)
+    GROUP BY host_id, environment_id
+  ) AS deleted_hosts
+  GROUP BY host_id, environment_id HAVING MAX(event_time) IS NOT NULL
+  ON DUPLICATE KEY UPDATE sla_lifecycle.id = sla_lifecycle.id;
+
+-- Insert a sla lifecycle create_time entry for all existing services with the LEAST timestamp found in either
+-- the sla_history_state or sla_history_downtime table, otherwise fallback to the current Unix timestamp.
+INSERT INTO sla_lifecycle (id, environment_id, host_id, service_id, create_time)
+  SELECT service.id,
+    service.environment_id,
+    service.host_id,
+    service.id,
+     -- In MySQL/MariaDB, LEAST() returns NULL if either event_time or downtime_start is NULL, which is not
+     -- desirable for our use cases. So we need to work around this behaviour by nesting some COALESCE() calls.
+    COALESCE(LEAST(COALESCE(MIN(event_time), MIN(downtime_start)), COALESCE(MIN(downtime_start), MIN(event_time))), UNIX_TIMESTAMP() * 1000) AS create_time
+  FROM service
+    LEFT JOIN sla_history_state shs on service.id = shs.service_id
+    LEFT JOIN sla_history_downtime shd on service.id = shd.service_id
+  GROUP BY service.id, service.host_id, service.environment_id
+  ON DUPLICATE KEY UPDATE sla_lifecycle.id = sla_lifecycle.id;
+
+-- Insert a sla lifecycle deleted entry for all not existing hosts with the GREATEST timestamp
+-- found in either the sla_history_state or sla_history_downtime table.
+INSERT INTO sla_lifecycle (id, environment_id, host_id, service_id, delete_time)
+  SELECT service_id AS id,
+    environment_id,
+    host_id,
+    service_id,
+    MAX(event_time) AS delete_time
+  FROM (SELECT service_id, environment_id, host_id, MAX(event_time) AS event_time
+    FROM sla_history_state
+      WHERE service_id IS NOT NULL AND NOT EXISTS(SELECT 1 FROM service WHERE id = service_id)
+    GROUP BY service_id, environment_id, host_id
+    UNION ALL
+    SELECT service_id, environment_id, host_id, MAX(downtime_end) AS event_time
+    FROM sla_history_downtime
+      WHERE service_id IS NOT NULL AND NOT EXISTS(SELECT 1 FROM service WHERE id = service_id)
+    GROUP BY service_id, environment_id, host_id
+  ) AS deleted_services
+  GROUP BY service_id, environment_id, host_id HAVING MAX(event_time) IS NOT NULL
+  ON DUPLICATE KEY UPDATE sla_lifecycle.id = sla_lifecycle.id;
+
 INSERT INTO icingadb_schema (version, timestamp)
   VALUES (6, UNIX_TIMESTAMP() * 1000);
