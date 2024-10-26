4c38be1fc07a819345bfffb8a5c539176debbacd
Eric Lippmann <eric.lippmann@icinga.com>
2024-10-26 20:14:13 +0200

    Support loading configuration from both YAML files and env vars

--- internal/command/command.go
+++ internal/command/command.go
@@ -10,6 +10,7 @@ import (
 	icingadbconfig "github.com/icinga/icingadb/internal/config"
 	"github.com/icinga/icingadb/pkg/icingaredis/telemetry"
 	"github.com/pkg/errors"
+	"io/fs"
 	"os"
 	"time"
 )
@@ -37,8 +38,48 @@ func New() *Command {
 		os.Exit(0)
 	}
 
+	// This function supports configuration loading in three scenarios:
+	//
+	// 1. Load configuration solely from YAML files when no relevant environment variables are set.
+	//
+	// 2. Combine YAML file and environment variable configurations, allowing environment variables
+	//    to supplement or override possible incomplete YAML configurations.
+	//
+	// 3. Load entirely from environment variables if the default YAML config file is absent and
+	//    no specific config path is provided by the user.
 	var cfg icingadbconfig.Config
-	if err := config.FromYAMLFile(flags.Config, &cfg); err != nil {
+	var configPath string
+	if flags.Config != "" {
+		configPath = flags.Config
+	} else {
+		configPath = icingadbconfig.DefaultConfigPath
+	}
+
+	if err := config.FromYAMLFile(configPath, &cfg); err != nil {
+		if errors.Is(err, config.ErrInvalidArgument) {
+			panic(err)
+		}
+
+		// Allow continuation with FromEnv by handling:
+		//
+		// - ErrInvalidConfiguration:
+		//   The configuration may be incomplete and will be revalidated in FromEnv.
+		//
+		// - Non-existent file errors:
+		//   If no explicit config path is set, fallback to environment variables is allowed.
+		configIsInvalid := errors.Is(err, config.ErrInvalidConfiguration)
+		defaultConfigFileDoesNotExist := errors.Is(err, fs.ErrNotExist) &&
+			configPath == icingadbconfig.DefaultConfigPath
+		if !(configIsInvalid || defaultConfigFileDoesNotExist) {
+			utils.PrintErrorThenExit(err, 1)
+		}
+	}
+
+	// Call FromEnv regardless of the outcome from FromYAMLFile.
+	// If no environment variables are set, configuration relies entirely on YAML.
+	// Otherwise, environment variables can supplement, override YAML settings, or serve as the sole source.
+	// FromEnv also includes validation, ensuring completeness after considering both sources.
+	if err := config.FromEnv(&cfg, config.EnvOptions{Prefix: "ICINGADB_"}); err != nil {
 		if errors.Is(err, config.ErrInvalidArgument) {
 			panic(err)
 		}
--- internal/config/config.go
+++ internal/config/config.go
@@ -10,12 +10,15 @@ import (
 	"time"
 )
 
+// DefaultConfigPath specifies the default location of Icinga DB's config.yml for package installations.
+const DefaultConfigPath = "/etc/icingadb/config.yml"
+
 // Config defines Icinga DB config.
 type Config struct {
-	Database  database.Config `yaml:"database"`
-	Redis     redis.Config    `yaml:"redis"`
-	Logging   logging.Config  `yaml:"logging"`
-	Retention RetentionConfig `yaml:"retention"`
+	Database  database.Config `yaml:"database" envPrefix:"DATABASE_"`
+	Redis     redis.Config    `yaml:"redis" envPrefix:"REDIS_"`
+	Logging   logging.Config  `yaml:"logging" envPrefix:"LOGGING_"`
+	Retention RetentionConfig `yaml:"retention" envPrefix:"RETENTION_"`
 }
 
 func (c *Config) SetDefaults() {
@@ -50,7 +53,7 @@ type Flags struct {
 	// Version decides whether to just print the version and exit.
 	Version bool `long:"version" description:"print version and exit"`
 	// Config is the path to the config file
-	Config string `short:"c" long:"config" description:"path to config file" required:"true" default:"/etc/icingadb/config.yml"`
+	Config string `short:"c" long:"config" description:"path to config file"`
 }
 
 // RetentionConfig defines configuration for history retention.
