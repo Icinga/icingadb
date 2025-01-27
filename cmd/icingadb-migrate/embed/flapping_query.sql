f94a65f459a088afbfb3b2f03dfed0949eda529b
Alvar Penning <alvar.penning@icinga.com>
2025-01-27 10:40:38 +0100

    icingadb-migrate: Explicit AS in SELECT
    By introducing an explicit "AS" to set output names in the SELECT column
    list, there are no issues with reserved names. Unfortunately, this
    happened on PostgreSQL in the older version 13 with the reserved name
    "name". Adding "AS" mitigates this issue.
    
    Furthermore, I have put each column name in its own line for the SELECT
    queries to ease the readability of the query itself and of future diffs.
    
    Fixes #884.

--- cmd/icingadb-migrate/embed/flapping_query.sql
+++ cmd/icingadb-migrate/embed/flapping_query.sql
@@ -1,6 +1,14 @@
-SELECT fh.flappinghistory_id, UNIX_TIMESTAMP(fh.event_time) event_time,
-  fh.event_time_usec, fh.event_type, fh.percent_state_change, fh.low_threshold,
-  fh.high_threshold, o.objecttype_id, o.name1, COALESCE(o.name2, '') name2
+SELECT
+  fh.flappinghistory_id,
+  UNIX_TIMESTAMP(fh.event_time) AS event_time,
+  fh.event_time_usec,
+  fh.event_type,
+  fh.percent_state_change,
+  fh.low_threshold,
+  fh.high_threshold,
+  o.objecttype_id,
+  o.name1,
+  COALESCE(o.name2, '') AS name2
 FROM icinga_flappinghistory fh USE INDEX (PRIMARY)
 INNER JOIN icinga_objects o ON o.object_id=fh.object_id
 WHERE fh.flappinghistory_id BETWEEN :fromid AND :toid
