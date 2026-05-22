34f0171415513f51090ae443902ee3bd26c85884
Alvar Penning <alvar.penning@icinga.com>
2026-05-22 13:27:48 +0200

    notifications: Attribute Negotiation
    Initial support for attribute negotiation, the big change with Icinga
    Notifications v1.0.
    
    Now, the notifications.Client does not evaluate the rules by itself
    anymore, but passes a subset of available information to Icinga
    Notifications. From there on, Icinga Notifications might return multiple
    JSONPath objects, which are being used to enrich the submitted event.
    
    This change required an adjustment to the icinga-go-library, which was
    bumped here to its PR branch.

---
+++ pkg/notifications/fetch_test.go
@@ -0,0 +1,89 @@
+package notifications
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/icinga/icinga-go-library/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/theory/jsonpath/spec"
+)
+
+func Test_relations_complete(t *testing.T) {
+	t.Run("demo provider", func(t *testing.T) {
+		rel := &relations{
+			hostId: types.Binary([]byte{0xc0, 0xff, 0xee}),
+
+			provider: func(_ context.Context, rel *relations, segments []*spec.Segment) error {
+				if !strings.Contains(fmt.Sprintf("%+v", segments), "vars") {
+					// Initial call: construct host object.
+					rel.Host = &icingaObject{
+						Name:        "example.com",
+						DisplayName: "example.com",
+					}
+					rel.completeRelations = append(rel.completeRelations, "host.name", "host.display_name")
+				} else {
+					// Subsequent call: populate custom vars.
+					rel.Host.Vars = map[string]any{
+						"env": "prod",
+						"os":  "Hannah Montana Linux",
+					}
+					rel.completeRelations = append(rel.completeRelations, "host.vars")
+				}
+				return nil
+			},
+		}
+
+		rel.populateObject()
+
+		assert.NoError(t, rel.complete(t.Context(), "$.host.name"))
+		assert.Equal(t,
+			&icingaObject{
+				Name:        "example.com",
+				DisplayName: "example.com",
+			},
+			rel.Host)
+		assert.Equal(t, []string{"object.type", "host.name", "host.display_name"}, rel.completeRelations)
+
+		assert.NoError(t, rel.complete(t.Context(), "$.host.vars"))
+		assert.Equal(t,
+			&icingaObject{
+				Name:        "example.com",
+				DisplayName: "example.com",
+				Vars:        map[string]any{"env": "prod", "os": "Hannah Montana Linux"},
+			},
+			rel.Host)
+		assert.Equal(t, []string{"object.type", "host.name", "host.display_name", "host.vars"}, rel.completeRelations)
+
+		jsonOut, err := json.Marshal(rel.asMap())
+		assert.NoError(t, err)
+		assert.JSONEq(t,
+			`{
+				"object": {"type": "host"},
+				"host": {
+					"name": "example.com",
+					"display_name": "example.com",
+					"vars": {
+						"env": "prod",
+						"os": "Hannah Montana Linux"
+					}
+				}
+			}`,
+			string(jsonOut))
+	})
+
+	t.Run("jsonpath invalid", func(t *testing.T) {
+		rel := &relations{
+			provider: func(_ context.Context, _ *relations, _ []*spec.Segment) error {
+				t.Fatal("provider should not have been called")
+				return nil
+			},
+		}
+
+		err := rel.complete(t.Context(), "$[invalid")
+		assert.Error(t, err)
+	})
+}
