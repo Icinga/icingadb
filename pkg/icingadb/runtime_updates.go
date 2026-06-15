a708c6e76b9a53e5b00a008a235de06cf5faaf07
Yonas Habteab <yonas.habteab@icinga.com>
2026-06-15 10:50:24 +0200

    notifications: consume state change events from runtime updates

--- pkg/icingadb/runtime_updates.go
+++ pkg/icingadb/runtime_updates.go
@@ -10,17 +10,16 @@ import (
 	"github.com/icinga/icinga-go-library/redis"
 	"github.com/icinga/icinga-go-library/strcase"
 	"github.com/icinga/icinga-go-library/structify"
+	"github.com/icinga/icinga-go-library/types"
 	"github.com/icinga/icingadb/pkg/common"
 	"github.com/icinga/icingadb/pkg/contracts"
 	v1 "github.com/icinga/icingadb/pkg/icingadb/v1"
 	"github.com/icinga/icingadb/pkg/icingaredis/telemetry"
 	"github.com/pkg/errors"
-	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
+	"maps"
 	"reflect"
-	"strconv"
-	"strings"
 	"sync"
 )
 
@@ -57,50 +56,124 @@ func (r *RuntimeUpdates) ClearStreams(ctx context.Context) (config, state redis.
 	return
 }
 
-// Sync synchronizes runtime update streams from s.redis to s.db and deletes the original data on success.
-// Note that Sync must be only be called configuration synchronization has been completed.
-// allowParallel allows synchronizing out of order (not FIFO).
+// Sync synchronizes runtime update streams from r.redis to r.db and deletes the original data on success.
+//
+// The options parameter can be used to specify additional options for the synchronization, such as allowing
+// parallel execution of updates for the same entity type (bulk DB upsert and delete ops) or providing a callback
+// that is called for each Redis stream message of type "upsert" in parallel with the main database upsert operations
+// for the same entity type.
+//
+// Note that Sync must only be called after configuration synchronization has been completed.
 func (r *RuntimeUpdates) Sync(
-	ctx context.Context, factoryFuncs []database.EntityFactoryFunc, streams redis.Streams, allowParallel bool,
+	ctx context.Context,
+	factoryFuncs []database.EntityFactoryFunc,
+	streams redis.Streams,
+	options ...RUOption,
 ) error {
+	if len(streams) != 1 {
+		return errors.New("streams must contain exactly one stream key for the runtime updates")
+	}
+
+	type messageByKey map[string]chan<- redis.XMessage
+	// xReads is a slice of length 2, where the first element is the map of each entity keys to channels for the
+	// main sync, and the second element is the map for the additional synchronization with custom onUpsert callback
+	// (if specified). This separation is necessary because we want to start a separate xRead goroutine for each sync,
+	// and each xRead goroutine needs its own set of channels to send the messages to.
+	xReads := make([]messageByKey, 2)
+
+	opts := new(RUOptions)
+	for _, opt := range options {
+		opt(opts)
+	}
+
 	g, ctx := errgroup.WithContext(ctx)
+	prepareForSync := func(s *common.SyncSubject, serializerCh <-chan any) (chan<- redis.XMessage, <-chan database.Entity, <-chan any) {
+		var upsertEntities chan database.Entity
+		var updateMessages chan redis.XMessage
+		var deleteIds chan any
 
-	updateMessagesByKey := make(map[string]chan<- redis.XMessage)
-
-	for _, factoryFunc := range factoryFuncs {
-		s := common.NewSyncSubject(factoryFunc)
-		stat := getCounterForEntity(s.Entity())
-
-		updateMessages := make(chan redis.XMessage, r.redis.Options.XReadCount)
-		upsertEntities := make(chan database.Entity, r.redis.Options.XReadCount)
-		deleteIds := make(chan any, r.redis.Options.XReadCount)
-
-		var upsertedFifo chan database.Entity
-		var deletedFifo chan any
-		var upsertCount int
-		var deleteCount int
-		upsertStmt, upsertPlaceholders := r.db.BuildUpsertStmt(s.Entity())
-		if !allowParallel {
-			upsertedFifo = make(chan database.Entity, 1)
-			deletedFifo = make(chan any, 1)
-			upsertCount = 1
-			deleteCount = 1
+		// Don't use buffered channels if a serializer channel is provided, because the structifyStream goroutine will
+		// block on the serializer channel after each message dispatch to the respective upsertEntities or deleteIds
+		// channel. So, we don't need to unnecessarily use huge buffers in that case!
+		if serializerCh == nil {
+			updateMessages = make(chan redis.XMessage, r.redis.Options.XReadCount)
+			upsertEntities = make(chan database.Entity, r.redis.Options.XReadCount)
+			deleteIds = make(chan any, r.redis.Options.XReadCount)
 		} else {
-			upsertCount = r.db.BatchSizeByPlaceholders(upsertPlaceholders)
-			deleteCount = r.db.Options.MaxPlaceholdersPerStatement
+			updateMessages = make(chan redis.XMessage)
+			upsertEntities = make(chan database.Entity)
+			deleteIds = make(chan any)
 		}
 
-		updateMessagesByKey[fmt.Sprintf("icinga:%s", strcase.Delimited(s.Name(), ':'))] = updateMessages
-
-		r.logger.Debugf("Syncing runtime updates of %s", s.Name())
-
 		g.Go(structifyStream(
-			ctx, updateMessages, upsertEntities, upsertedFifo, deleteIds, deletedFifo,
+			ctx, updateMessages, upsertEntities, deleteIds, serializerCh,
 			structify.MakeMapStructifier(
 				reflect.TypeOf(s.Entity()).Elem(),
 				"json",
 				contracts.SafeInit),
 		))
+		return updateMessages, upsertEntities, deleteIds
+	}
+
+	for _, factoryFunc := range factoryFuncs {
+		s := common.NewSyncSubject(factoryFunc)
+		stat := getCounterForEntity(s.Entity())
+
+		r.logger.Debugf("Syncing runtime updates of %s", s.Name())
+
+		key := fmt.Sprintf("icinga:%s", strcase.Delimited(s.Name(), ':'))
+
+		if opts.upsertFn != nil {
+			r.logger.Debugf("Starting additional sync with custom onUpsert callback for %s", s.Name())
+
+			serializerCh := make(chan any)
+			updateMessages, upsertEntities, deleteIds := prepareForSync(s, serializerCh)
+			if xReads[1] == nil {
+				xReads[1] = make(messageByKey)
+			}
+			xReads[1][key] = updateMessages
+
+			g.Go(func() error {
+				defer close(serializerCh)
+
+				for {
+					select {
+					case <-ctx.Done():
+						return ctx.Err()
+					case entity, ok := <-upsertEntities:
+						if !ok {
+							return nil
+						}
+						if err := opts.upsertFn(ctx, entity); err != nil {
+							return errors.Wrapf(err, "onUpsert callback failed for entity with ID %v", entity.ID())
+						}
+					case _, ok := <-deleteIds:
+						if !ok {
+							return nil
+						}
+						// Nothing to do for deletes in the onUpsert callback, but we need to consume
+						// the channel to avoid blocking the structifyStream goroutine.
+					}
+
+					select {
+					case <-ctx.Done():
+						return ctx.Err()
+					case serializerCh <- struct{}{}:
+					}
+				}
+			})
+		}
+
+		var serializerCh chan any
+		if !opts.allowParallel {
+			serializerCh = make(chan any)
+		}
+
+		updateMessages, upsertEntities, deleteIds := prepareForSync(s, serializerCh)
+		if xReads[0] == nil {
+			xReads[0] = make(messageByKey)
+		}
+		xReads[0][key] = updateMessages
 
 		g.Go(func() error {
 			var counter com.Counter
@@ -116,8 +189,14 @@ func (r *RuntimeUpdates) Sync(
 			onSuccess := []database.OnSuccess[database.Entity]{
 				database.OnSuccessIncrement[database.Entity](&counter), database.OnSuccessIncrement[database.Entity](stat),
 			}
-			if !allowParallel {
-				onSuccess = append(onSuccess, database.OnSuccessSendTo(upsertedFifo))
+
+			var upsertCount int
+			upsertStmt, upsertPlaceholders := r.db.BuildUpsertStmt(s.Entity())
+			if !opts.allowParallel {
+				upsertCount = 1
+				onSuccess = append(onSuccess, database.OnSuccessApplyAndSendTo(serializerCh, func(e database.Entity) any { return e }))
+			} else {
+				upsertCount = r.db.BatchSizeByPlaceholders(upsertPlaceholders)
 			}
 
 			return r.db.NamedBulkExec(
@@ -136,8 +215,12 @@ func (r *RuntimeUpdates) Sync(
 			sem := r.db.GetSemaphoreForTable(database.TableName(s.Entity()))
 
 			onSuccess := []database.OnSuccess[any]{database.OnSuccessIncrement[any](&counter), database.OnSuccessIncrement[any](stat)}
-			if !allowParallel {
-				onSuccess = append(onSuccess, database.OnSuccessSendTo(deletedFifo))
+			var deleteCount int
+			if !opts.allowParallel {
+				deleteCount = 1
+				onSuccess = append(onSuccess, database.OnSuccessSendTo(serializerCh))
+			} else {
+				deleteCount = r.db.Options.MaxPlaceholdersPerStatement
 			}
 
 			return r.db.BulkExec(ctx, r.db.BuildDeleteStmt(s.Entity()), deleteCount, sem, deleteIds, onSuccess...)
@@ -156,9 +239,9 @@ func (r *RuntimeUpdates) Sync(
 		r.logger.Debug("Syncing runtime updates of " + cv.Name())
 		r.logger.Debug("Syncing runtime updates of " + cvFlat.Name())
 
-		updateMessagesByKey["icinga:"+strcase.Delimited(cv.Name(), ':')] = updateMessages
+		xReads[0]["icinga:"+strcase.Delimited(cv.Name(), ':')] = updateMessages
 		g.Go(structifyStream(
-			ctx, updateMessages, upsertEntities, nil, deleteIds, nil,
+			ctx, updateMessages, upsertEntities, deleteIds, nil,
 			structify.MakeMapStructifier(
 				reflect.TypeOf(cv.Entity()).Elem(),
 				"json",
@@ -227,16 +310,40 @@ func (r *RuntimeUpdates) Sync(
 		})
 	}
 
-	g.Go(r.xRead(ctx, updateMessagesByKey, streams))
+	// Since all xRead goroutines are going to consume messages from the same stream independently, we are only
+	// allowed to send XDel commands after we've successfully dispatched all messages to the corresponding
+	// updateMessages channels. For the database ops, it doesn't really matter when the msgs are acked, but for
+	// the onUpsert callback, the per type updates are processed sequentially, so the xRead will block each time
+	// it tries to send a message to the chOuts channel until the callback has processed the previous one. When
+	// the callback fails to process the previous one (which we already deleted from the stream), we'll either
+	// trigger a HA handover or crash Icinga DB fatally, in which case losing that message is not a big deal.
+	var xRedisMessageAcks []<-chan string
+	for _, chOuts := range xReads {
+		if chOuts == nil {
+			continue
+		}
+		ackMessageCh := make(chan string)
+		xRedisMessageAcks = append(xRedisMessageAcks, ackMessageCh)
+
+		g.Go(r.xRead(ctx, chOuts, ackMessageCh, maps.Clone(streams)))
+	}
+
+	g.Go(func() error { return r.redis.XDelOnAllConsumersAck(ctx, streams.Option()[0], xRedisMessageAcks...) })
 
 	return g.Wait()
 }
 
 // xRead reads from the runtime update streams and sends the data to the corresponding updateMessages channel.
 // The updateMessages channel is determined by a "redis_key" on each redis message.
-func (r *RuntimeUpdates) xRead(ctx context.Context, updateMessagesByKey map[string]chan<- redis.XMessage, streams redis.Streams) func() error {
+func (r *RuntimeUpdates) xRead(
+	ctx context.Context,
+	updateMessagesByKey map[string]chan<- redis.XMessage,
+	acknowledgementOutCh chan<- string,
+	streams redis.Streams,
+) func() error {
 	return func() error {
 		defer func() {
+			close(acknowledgementOutCh)
 			for _, updateMessages := range updateMessagesByKey {
 				close(updateMessages)
 			}
@@ -251,7 +358,6 @@ func (r *RuntimeUpdates) xRead(ctx context.Context, updateMessagesByKey map[stri
 				return errors.Wrap(err, "can't read runtime updates")
 			}
 
-			pipe := r.redis.Pipeline()
 			for _, stream := range rs {
 				var id string
 
@@ -273,25 +379,14 @@ func (r *RuntimeUpdates) xRead(ctx context.Context, updateMessagesByKey map[stri
 					case <-ctx.Done():
 						return ctx.Err()
 					}
-				}
 
-				tsAndSerial := strings.Split(id, "-")
-				if s, err := strconv.ParseUint(tsAndSerial[1], 10, 64); err == nil {
-					tsAndSerial[1] = strconv.FormatUint(s+1, 10)
-				}
-
-				pipe.XTrimMinIDApprox(ctx, stream.Stream, strings.Join(tsAndSerial, "-"), 0)
-				streams[stream.Stream] = id
-			}
-
-			if cmds, err := pipe.Exec(ctx); err != nil {
-				r.logger.Errorw("Can't execute Redis pipeline", zap.Error(errors.WithStack(err)))
-			} else {
-				for _, cmd := range cmds {
-					if cmd.Err() != nil {
-						r.logger.Errorw("Can't trim runtime updates stream", zap.Error(redis.WrapCmdErr(cmd)))
+					select {
+					case acknowledgementOutCh <- message.ID:
+					case <-ctx.Done():
+						return ctx.Err()
 					}
 				}
+				streams[stream.Stream] = id
 			}
 		}
 	}
@@ -302,34 +397,27 @@ func (r *RuntimeUpdates) xRead(ctx context.Context, updateMessagesByKey map[stri
 // Converted entities are inserted into the upsertEntities or deleteIds channel depending on the "runtime_type" message field.
 func structifyStream(
 	ctx context.Context,
-	updateMessages <-chan redis.XMessage,
-	upsertEntities chan<- database.Entity,
-	upserted <-chan database.Entity,
-	deleteIds chan<- any,
-	deleted <-chan any,
+	messagesInCh <-chan redis.XMessage,
+	upsertEntitiesOutCh chan<- database.Entity,
+	deleteIdsOutCh chan<- any,
+	serializerInCh <-chan any,
 	structifier structify.MapStructifier,
 ) func() error {
-	if upserted == nil {
-		ch := make(chan database.Entity)
-		close(ch)
-		upserted = ch
-	}
-
-	if deleted == nil {
+	if serializerInCh == nil {
 		ch := make(chan any)
 		close(ch)
-		deleted = ch
+		serializerInCh = ch
 	}
 
 	return func() error {
 		defer func() {
-			close(upsertEntities)
-			close(deleteIds)
+			close(upsertEntitiesOutCh)
+			close(deleteIdsOutCh)
 		}()
 
 		for {
 			select {
-			case message, ok := <-updateMessages:
+			case message, ok := <-messagesInCh:
 				if !ok {
 					return nil
 				}
@@ -351,34 +439,56 @@ func structifyStream(
 
 				if runtimeType == "upsert" {
 					select {
-					case upsertEntities <- entity:
-					case <-ctx.Done():
-						return ctx.Err()
-					}
-
-					select {
-					case <-upserted:
+					case upsertEntitiesOutCh <- entity:
 					case <-ctx.Done():
 						return ctx.Err()
 					}
 				} else if runtimeType == "delete" {
 					select {
-					case deleteIds <- entity.ID():
-					case <-ctx.Done():
-						return ctx.Err()
-					}
-
-					select {
-					case <-deleted:
+					case deleteIdsOutCh <- entity.ID():
 					case <-ctx.Done():
 						return ctx.Err()
 					}
 				} else {
 					return errors.Errorf("invalid runtime type: %s", runtimeType)
 				}
+
+				select {
+				case <-serializerInCh:
+				case <-ctx.Done():
+					return ctx.Err()
+				}
 			case <-ctx.Done():
 				return ctx.Err()
 			}
 		}
 	}
+}
+
+// RUOption defines an option for the [RuntimeUpdates.Sync] method.
+type RUOption func(*RUOptions)
+
+// RUUpsertFunc defines the type of the callback that can be provided to the [RuntimeUpdates.Sync] method via the [WithRUUpsert] option.
+type RUUpsertFunc func(context.Context, database.Entity) error
+
+// RUOptions defines options for the [RuntimeUpdates.Sync] method.
+type RUOptions struct {
+	allowParallel bool
+	upsertFn      RUUpsertFunc
+}
+
+// WithAllowParallel allows parallel execution of runtime updates for the same entity type.
+func WithAllowParallel() RUOption {
+	return func(opts *RUOptions) { opts.allowParallel = true }
+}
+
+// WithRUUpsert allows providing a callback that is called for each Redis stream message of type "upsert".
+//
+// If this option is used, [RuntimeUpdates.Sync] will start a separate xRead goroutine for the corresponding stream,
+// thus allowing the provided callback to run in parallel with the main database upsert operations for the same entity
+// type. Note that the callback is called for each message of type "upsert", regardless of the entity type, so it is
+// the responsibility of that callback to filter the entities if necessary. Also note that the callback must be safe
+// for concurrent execution, as it may be called in parallel with different types of entities.
+func WithRUUpsert(fn RUUpsertFunc) RUOption {
+	return func(opts *RUOptions) { opts.upsertFn = fn }
 }
